497ea2e2a42618588842275bc4d2c3a36dafd5c8
Yvonne Marneth <Yvonne.Marneth@risc-software.at>
2024-10-09 16:59:16 +0200

    Imlement data model

--- src/main/scala/expr/Expr.scala
+++ src/main/scala/expr/Expr.scala
@@ -1,7 +1,22 @@
 package expr
 
 sealed trait Expr
-// TODO
+
+case class Lit(v: Double) extends Expr
+
+case class Var(n: String) extends Expr
+
+sealed trait BinExpr(val left: Expr, val right: Expr) extends Expr
+
+case class Add(addend: Expr, augend: Expr) extends BinExpr(addend, augend)
+
+case class Mult(multiplend: Expr, multiplier: Expr) extends BinExpr(multiplend, multiplier)
+
+sealed trait UnyExpr(sub: Expr) extends Expr
+
+case class Min(s: Expr) extends UnyExpr(s)
+
+case class Rec(s: Expr) extends UnyExpr(s)
 
 def infix(expr: Expr): String = ??? // TODO:
 
